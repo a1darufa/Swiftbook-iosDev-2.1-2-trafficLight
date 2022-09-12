0e2ef4f5859483befa62ede1f68757c7c9c08648
Aidar <a1dar.ufa@gmail.com>
2022-09-12 16:08:07 +0500

    Coded ViewController

--- Swiftbook-iosDev-2.1-2-trafficLight/ViewController.swift
+++ Swiftbook-iosDev-2.1-2-trafficLight/ViewController.swift
@@ -9,12 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightLabel: UILabel!
+    @IBOutlet var yellowLightLabel: UILabel!
+    @IBOutlet var greenLightLabel: UILabel!
+    @IBOutlet var switchColorLabel: UIButton!
+    
+    enum Condition: Int {
+        case stop
+        case ready
+        case go
+        case caution
+        case attention
+    }
+    
+    var lightState = Condition.stop
+    
+    let lightOn = 1.0
+    let lightOff = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        prepareLight(for: redLightLabel)
+        prepareLight(for: yellowLightLabel)
+        prepareLight(for: greenLightLabel)
 
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func switchColorButton(_ sender: UIButton) {
+        switch lightState {
+        case .stop:
+            redLightLabel.alpha = lightOn
+            yellowLightLabel.alpha = lightOff
+            lightState = .ready
+            switchColorLabel.setTitle("NEXT", for: .normal)
+        case .ready:
+            yellowLightLabel.alpha = lightOn
+            lightState = .go
+        case .go:
+            redLightLabel.alpha = lightOff
+            yellowLightLabel.alpha = lightOff
+            greenLightLabel.alpha = lightOn
+            lightState = .caution
+        case .caution:
+            lightState = .attention
+// want to try add green light flashing
+        case .attention:
+            greenLightLabel.alpha = lightOff
+            yellowLightLabel.alpha = lightOn
+            lightState = .stop
+        }
+    }
+    
+    func prepareLight(for light: UILabel) {
+        light.layer.cornerRadius = light.frame.height / 2
+        light.clipsToBounds = true
+        light.alpha = lightOff
+    }
 }
 
